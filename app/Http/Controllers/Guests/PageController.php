32db80802fee64b35000c42a860e543402018310
mattemutti <mattemutti@gmail.com>
2024-05-09 15:48:20 +0200

    add pagecontroller and static page site

--- app/Http/Controllers/Guests/PageController.php
+++ app/Http/Controllers/Guests/PageController.php
@@ -7,5 +7,9 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+
+    public function index()
+    {
+        return view('welcome');
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,39 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
+@extends('layouts.app')
 
 
-
-    <div class="container">
-        <div class="row">
-            <div class="col">
-
-
-
-
-
-            </div>
-        </div>
-    </div>
-
-
-
-
-</body>
-
-</html>
+@section('content')
+    <h1>hello</h1>
+@endsection
