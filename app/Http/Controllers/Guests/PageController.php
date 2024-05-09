18052015142f9181afabd160f614207879808eec
mattemutti <mattemutti@gmail.com>
2024-05-09 16:01:07 +0200

    add link db movie in PageController

--- app/Http/Controllers/Guests/PageController.php
+++ app/Http/Controllers/Guests/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Guests;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -10,6 +11,10 @@ class PageController extends Controller
 
     public function index()
     {
+
+        $movies = Movie::all();
+        dd($movies);
+
         return view('guests.welcome');
     }
     public function about()
